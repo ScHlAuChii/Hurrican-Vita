230b4b07336708353cfcffd56248158a79460d49
Peter Mackay <mackay.pete+git@gmail.com>
2016-09-01 07:37:00 +0100

    Stub module loading.

--- Hurrican/Wrap/fmusic.cpp
+++ Hurrican/Wrap/fmusic.cpp
@@ -1,28 +1,44 @@
 #include "fmod.h"
 
+#include <assert.h>
+
+struct FMUSIC_MODULE
+{
+};
+
 signed char FMUSIC_FreeSong(FMUSIC_MODULE *mod)
 {
+	assert(mod != nullptr);
+	
+	delete mod;
+	
 	return 1;
 }
 
 signed char FMUSIC_GetPaused(FMUSIC_MODULE *mod)
 {
+	assert(mod != nullptr);
+	
 	return 0;
 }
 
 signed char FMUSIC_IsFinished(FMUSIC_MODULE *mod)
 {
+	assert(mod != nullptr);
+	
 	return 1;
 }
 
 signed char FMUSIC_IsPlaying(FMUSIC_MODULE *mod)
 {
+	assert(mod != nullptr);
+	
 	return 0;
 }
 
 FMUSIC_MODULE *FMUSIC_LoadSong(const char *name)
 {
-	return nullptr;
+	return new FMUSIC_MODULE;
 }
 
 FMUSIC_MODULE *FMUSIC_LoadSongEx(const char *name_or_data, int offset, int length, unsigned int mode, const int *samplelist, int samplelistnum)
@@ -32,16 +48,22 @@ FMUSIC_MODULE *FMUSIC_LoadSongEx(const char *name_or_data, int offset, int lengt
 
 signed char FMUSIC_PlaySong(FMUSIC_MODULE *mod)
 {
+	assert(mod != nullptr);
+	
 	return 1;
 }
 
 signed char FMUSIC_SetMasterVolume(FMUSIC_MODULE *mod, int volume)
 {
+	assert(mod != nullptr);
+	
 	return 1;
 }
 
 signed char FMUSIC_SetPaused(FMUSIC_MODULE *mod, signed char pause)
 {
+	assert(mod != nullptr);
+	
 	return 1;
 }
 
@@ -51,5 +73,7 @@ void FMUSIC_StopAllSongs()
 
 signed char FMUSIC_StopSong(FMUSIC_MODULE *mod)
 {
+	assert(mod != nullptr);
+	
 	return 1;
 }
