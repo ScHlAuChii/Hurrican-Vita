ee113852c63b5fef4d0e974db31a332a50efb70f
Peter Mackay <mackay.pete+git@gmail.com>
2016-09-06 18:17:44 +0100

    Use windowed mode by default.

--- Hurrican/SDL/main_sdl.cpp
+++ Hurrican/SDL/main_sdl.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 	
-	const int result = WinMain(nullptr, nullptr, argv[0], 0);
+	const int result = WinMain(nullptr, nullptr, "Hurrican.exe windowmode", 0);
 	
 	SDL_Quit();
 	return result;
