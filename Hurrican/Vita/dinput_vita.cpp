2fd9b8507e8a062b6a0c5a49133a6ded077058b4
Peter Mackay <mackay.pete+git@gmail.com>
2016-10-14 08:12:11 +0100

    First pass buttons-as-keyboard input.

--- Hurrican/Vita/dinput_vita.cpp
+++ Hurrican/Vita/dinput_vita.cpp
@@ -1,5 +1,13 @@
 #include "dinput.h"
 
+#include <psp2/ctrl.h>
+
+struct KeyBinding
+{
+	Keyboard_Device key;
+	unsigned int button;
+};
+
 class DirectInput : public IDirectInput8
 {
 public:
@@ -24,8 +32,29 @@ public:
 	HRESULT Unacquire() override;
 };
 
+static const KeyBinding key_bindings[] =
+{
+	{ DIK_DOWN, SCE_CTRL_DOWN },
+	{ DIK_END, SCE_CTRL_SELECT },
+	{ DIK_ESCAPE, SCE_CTRL_START },
+	{ DIK_LALT, SCE_CTRL_CROSS },
+	{ DIK_LCONTROL, SCE_CTRL_TRIANGLE },
+	{ DIK_LEFT, SCE_CTRL_LEFT },
+	{ DIK_LSHIFT, SCE_CTRL_TRIANGLE },
+	{ DIK_RCONTROL, SCE_CTRL_SQUARE },
+	{ DIK_RETURN, SCE_CTRL_START },
+	{ DIK_RIGHT, SCE_CTRL_RIGHT },
+	{ DIK_RSHIFT, SCE_CTRL_LTRIGGER },
+	{ DIK_SPACE, SCE_CTRL_RTRIGGER },
+	{ DIK_UP, SCE_CTRL_UP },
+};
+
+static const size_t key_binding_count = sizeof(key_bindings) / sizeof(key_bindings[0]);
+
 HRESULT DirectInput8Create(HINSTANCE hinst, DWORD dwVersion, REFIID riidltf, LPVOID *ppvOut, LPUNKNOWN punkOuter)
 {
+	sceCtrlSetSamplingMode(SCE_CTRL_MODE_ANALOG);
+	
 	*ppvOut = new DirectInput();
 	
 	return DI_OK;
@@ -71,7 +100,21 @@ HRESULT Keyboard::GetDeviceData(DWORD cbObjectData, LPDIDEVICEOBJECTDATA rgdod, 
 
 HRESULT Keyboard::GetDeviceState(DWORD cbData, LPVOID lpvData)
 {
+	uint8_t *const keys = static_cast<uint8_t *>(lpvData);
+	
 	memset(lpvData, 0, cbData);
+	
+	SceCtrlData pad = {};
+	sceCtrlPeekBufferPositive(0, &pad, 1);
+	
+	for (size_t i = 0; i < key_binding_count; ++i)
+	{
+		const KeyBinding &binding = key_bindings[i];
+		if (pad.buttons & binding.button)
+		{
+			keys[binding.key] = 0x80;
+		}
+	}
 	
 	return DI_OK;
 }
